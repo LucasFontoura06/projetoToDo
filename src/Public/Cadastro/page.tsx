ca26e1cc5ce6fd670ad127d8550e1c3645748389
Fabio <fabio@MacBook-Air-M2-de-Fabio.local>
2025-06-22 16:42:50 -0300

    telaCadastrov1

---
+++ src/Public/Cadastro/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Cadastro from './cadastro';
+
+function CadastroPage() {
+  return <Cadastro />;
+}
+
+export default CadastroPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 // Importando os componentes das páginas
 import HomePage from './Public/HomePage/homePage';
 import Login from './Public/Login/login';
+import Cadastro from './Public/Cadastro/cadastro';
 
 // Componente de rotas principal
 function AppRoutes() {
@@ -18,6 +19,9 @@ function AppRoutes() {
         
         {/* Rota da página de login */}
         <Route path="/login" element={<Login />} />
+        
+        {/* Rota da página de cadastro */}
+        <Route path="/cadastro" element={<Cadastro />} />
         
         {/* Rota para páginas não encontradas - redireciona para home */}
         <Route path="*" element={<Navigate to="/home" replace />} />
